2e4729fc6a0b302171f92d94dc88f035bd5f9338
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-06 15:20:35 +0000

    created a for loop

---
+++ looping_list.py
@@ -0,0 +1,5 @@
+subjects = ["calculus","algebra","biology","chemistry","environmental studies"]
+print("I have an A in adance, manifesting it")
+for subject in subjects:
+    print(subject.title() + ", I will ace you this semester I promise")
+    print("I have alraedy aced youuuuuu." + ".\n")
