43dc43b90de185759514782a5ab7b15bad548b2a
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-13 10:07:00 +0000

    added copying a list

---
+++ copying_list.py
@@ -0,0 +1,12 @@
+snacks = ["hardboiled egg", "popcorn", "yoghort", "trial mix"]
+friends_snacks = snacks[:]
+
+snacks.append("pizza")
+friends_snacks.append("ice cream")
+#adding our various preferences 
+
+print("my favorite snacks are:")
+print(snacks)
+
+print("\nMy friend's favorite snacks are:")
+print(friends_snacks)
