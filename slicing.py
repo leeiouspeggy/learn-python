009f35c5e75def44ef504604d2e1878b553c7d19
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-13 09:44:16 +0000

    added slicing list

---
+++ slicing.py
@@ -0,0 +1,11 @@
+names = ['Caroline', 'Rocklyn', 'Roselyn', 'Charles', 'Caleb', 'Adam']
+print(names[1:4])
+#just like range the last element isn't added just the one right before it
+
+print(names[:4])
+print(names[1:])
+#omitting the first will automatically print from the beginning
+#an vice versa works for the last 
+
+print(names[-3:])
+#This prints the names of the last three players
