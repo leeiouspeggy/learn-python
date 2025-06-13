fde5a81dea60625fe27d50930b5099b1cfd3db0e
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-13 09:51:11 +0000

    added looping through slice using for loop

---
+++ looping_through_slice.py
@@ -0,0 +1,7 @@
+names = ['Caroline', 'Rocklyn', 'Roselyn', 'Charles', 'Caleb', 'Adam']
+
+print("These are the names of the people who actively participated:")
+for  name in names[:3]:
+    print(name)
+#generate a particular number of elements in a list using slicing
+
