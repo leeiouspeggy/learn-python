e1425d0c6efe2496502261e101091e2231eb6234
Afriyie Ofori Boachie <afriyieoforiboachie@gmail.com>
2025-06-13 01:57:18 +0000

    added range function

--- range_list.py
+++ range_list.py
@@ -5,3 +5,8 @@ print(numbers)
 odd_numbers = list(range(1,11,2))
 print(odd_numbers)
 #allows you to generate numbers in a range in a specific pattern
+
+squares = []
+for value in range(1,11):
+    squares.append(value**2)
+    print(squares)
